f10f55e9375254371712c6ec4b900c6b259c5f5f
ws47033 <ws47033@ly.com>
2018-07-17 12:32:08 +0800

    feign集群

---
+++ service-feign/src/main/java/com/example/springcloud/HiController.java
@@ -0,0 +1,23 @@
+package com.example.springcloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ws47033
+ * @date 2018/7/17 12:28
+ */
+
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne( name );
+    }
+}
---
+++ service-feign/src/main/java/com/example/springcloud/SchedualServiceHi.java
@@ -0,0 +1,18 @@
+package com.example.springcloud;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author ws47033
+ * @date 2018/7/17 12:26
+ */
+
+@FeignClient("sevice-client-hi")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam("name") String name);
+}
